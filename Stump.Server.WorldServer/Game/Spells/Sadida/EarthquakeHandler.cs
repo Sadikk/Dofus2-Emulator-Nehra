f7fd71df912819b637550492f7da35e29b6c9e1f
Wax <waxmailer@gmail.com>
2016-05-15 21:09:26 +0200

    [Spells - Sadida] Adding Earthquake file

---
+++ Stump.Server.WorldServer/Game/Spells/Sadida/EarthquakeHandler.cs
@@ -0,0 +1,25 @@
+﻿using Stump.Core.Reflection;
+using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Spells.Casts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stump.Server.WorldServer.Game.Spells.Sadida
+{
+    class EarthquakeHandler : DefaultSpellCastHandler
+    {
+        public EarthquakeHandler(FightActor caster, Spell spell, Cell targetedCell, bool critical) : base(caster, spell, targetedCell, critical)
+        {
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
